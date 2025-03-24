2800be935e3983a49e0786db623f59c1b4a78cbe
lukas2710 <168042011+lukas2710@users.noreply.github.com>
2025-03-24 22:07:10 +0100

    Add files via upload
    Signed-off-by: lukas2710 <168042011+lukas2710@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const films = [
-  { name: "Charm City Kings", link: "../action/charm city king.html", image: "https://fr.web.img3.acsta.net/pictures/20/06/29/13/00/2524555.jpg", description: "Mouse rêve d'intégrer un club de motos et de trouver sa place dans Baltimore." },
+  { name: "Charm City Kings", link: "../action/charm cityking.html", image: "https://fr.web.img3.acsta.net/pictures/20/06/29/13/00/2524555.jpg", description: "Mouse rêve d'intégrer un club de motos et de trouver sa place dans Baltimore." },
   { name: "After", link: "../drame/after 1.html", image: "https://fr.web.img3.acsta.net/pictures/19/04/03/17/19/3475089.jpg", description: "L'histoire de Tessa, une jeune étudiante, et Hardin, un étudiant rebelle." },
   { name: "The Matrix", link: "the-matrix.html", image: "https://image.tmdb.org/t/p/w500/1bTlYlQLhfFe5F2kxY8Di3UpmlB.jpg", description: "Un hacker découvre la vérité sur la réalité et un monde simulé." },
   { name: "Inception", link: "inception.html", image: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg", description: "Un voleur peut pénétrer dans les rêves des gens pour voler leurs secrets." }
